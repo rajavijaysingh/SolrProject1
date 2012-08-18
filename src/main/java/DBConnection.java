ea3312cca9ddd3f7053bb7d489f37478a2769a59
rajeshp <pantularajesh@gmail.com>
2012-08-19 04:40:46 +0530

    Adding Connection class and categories display page

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rajeshp
+ * Date: 19/8/12
+ * Time: 4:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DBConnection {
+
+    public static Connection getConnection()
+    {
+        Connection con=null;
+        try
+        {
+
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        con = DriverManager.getConnection("jdbc:mysql://localhost/solrdb","root","rhino");
+
+        System.out.println("Connected to database");
+
+        }
+        catch(ClassNotFoundException ce)
+        {
+            ce.printStackTrace();
+        }
+        catch(SQLException se)
+        {
+            se.printStackTrace();
+        }
+
+
+       return con;
+
+    }
+
+
+
+}
---
+++ src/main/webapp/categories.jsp
@@ -0,0 +1,17 @@
+<html>
+<head>
+</head>
+<body>
+
+<h1 align="center"> Categories List </h1>
+
+<%
+
+
+%>
+
+
+</body>
+
+
+</html>
